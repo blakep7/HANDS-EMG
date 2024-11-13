5cd1418a2df8a844994645468091ff91571f8f3d
marosokn <marosokn@gmail.com>
2024-11-13 02:40:02 -0800

    this includes some basic data and scripts
    excludes subject 1, exercise 1 raw data... data size is too large to commit, maybe a drive link? zip?
    
    Introductory script for model in tensorflow
    
    custom matlab script for windowing the Ninapro data.

---
+++ machine learning/window_data.m
@@ -0,0 +1,52 @@
+function [windows, winStim, winRep] = window_data(emg, stimulus, repetition, winsize, wininc)
+    % Written by Noah Marosok 
+    % 
+    % This function only performs windowing on the input signals 
+    % It validates each window based on unique stimulus and repetition vals
+    %
+    % Inputs:
+    %   emg       - EMG data (can be multichannel)
+    %   stimulus  - Corresponding stimulus labels (int8)
+    %   repetition- Corresponding repetition labels
+    %   winsize   - Size of each window (samples)
+    %   wininc    - Increment between consecutive windows (samples)
+    %
+    % Outputs:
+    %   windows   - Cell array containing the valid windows from emg
+    %   winStim   - Vector of stimulus labels for each valid window
+    %   winRep    - Vector of repetition labels for each valid window
+
+    numwin = floor((size(emg, 1) - winsize) / wininc) + 1;
+    nSignals = size(emg, 2);
+
+
+    windows = cell(numwin, 1);
+    winStim = zeros(numwin, 1);
+    winRep = zeros(numwin, 1);
+    validWindowCount = 0;
+
+    
+    for winInd = 1:numwin
+        startIdx = (winInd - 1) * wininc + 1;
+        endIdx = startIdx + winsize - 1;
+
+        % extract current data
+        curWin = emg(startIdx:endIdx, :);
+        curStimWin = stimulus(startIdx:endIdx);
+        curRepWin = repetition(startIdx:endIdx);
+
+        % check for uniqueness
+        if numel(unique(curStimWin)) == 1 && numel(unique(curRepWin)) == 1
+            % store window and corresponding labels
+            validWindowCount = validWindowCount + 1;
+            windows{validWindowCount} = curWin;
+            winStim(validWindowCount) = curStimWin(1);
+            winRep(validWindowCount) = curRepWin(1);
+        end
+    end
+
+    % remove unused space
+    windows(validWindowCount + 1:end) = [];
+    winStim(validWindowCount + 1:end) = [];
+    winRep(validWindowCount + 1:end) = [];
+end
