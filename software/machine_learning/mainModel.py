27524637bfadb9a4aa7a5b29fc0f46172f1197b4
marosokn <marosokn@gmail.com>
2025-02-26 16:27:15 -0800

    data processing
    please no delete oh gitGod

--- software/machine_learning/mainModel.py
+++ software/machine_learning/mainModel.py
@@ -1,65 +1,59 @@
 import tensorflow as tf
 import numpy as np
 import pandas as pd #keep pandas here for now
+from sklearn.preprocessing import OneHotEncoder
 #=======hyperparameters
-num_features = 13 #discuss potential additions... refer to paper blake posted.. maybe add new metrics?
-#perform a PCA to see which features I can exclude to make lighter  
-num_classes = 17   
+num_features = 12 * 12
+num_classes = 5
 learning_rate = 0.001
-epochs = 10
-batch_size = 32
+epochs = 1
+batch_size = 256
+#===================
+dataPath = './dataForML.csv'
+data = pd.read_csv(dataPath)
+labels_to_keep = {0 , 13 ,9 , 6 , 16}
+data = data[data["Yval"].isin(labels_to_keep)]
 
-#this is dummy data until I get all data processed
-X_train = 0#FIXME # I think 80/20 split is good... shuffle samples to get an even training
-y_train = 0#FIXME   It should be a pretty long matrix that contains all ~13 features for each window
-                 #from every subject. Expect an mxn matrix with m = (rawDataLength / 2.1) * numSubjects
-                 # and n = ~13 
+X = data.drop(columns=['subjNum', 'Yval'])
+y = data['Yval']
+y = y[y.isin([0, 13, 9, 6, 16])]
 
-X_test = 0#    # 20 percent of x data
-y_test = 0 #FIXME
+encoder = OneHotEncoder(sparse_output=False)
+y = encoder.fit_transform(y.values.reshape(-1, 1))
 
-#build model
-model = tf.keras.Sequential([
-    tf.keras.layers.InputLayer(input_shape=(num_features,)),
-    tf.keras.layers.Dense(32, activation='relu'),  
-    tf.keras.layers.Dense(16, activation='relu'),  
-    tf.keras.layers.Dense(num_classes, activation='softmax') 
-])
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42, shuffle=False)
 
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate), #compiling the model
+
+X_train = X_train.to_numpy()
+X_test = X_test.to_numpy()
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+
+##from sklearn.decomposition import PCA
+#pca = PCA(n_components=36)  # Reduce from 48 to 36
+#X_train = pca.fit_transform(X_train)
+#X_test = pca.transform(X_test)
+
+
+inputs = tf.keras.Input(shape=(num_features,))
+x = tf.keras.layers.Dense(64, activation='relu')(inputs)
+x = tf.keras.layers.Dense(32, activation='relu')(x)
+x = tf.keras.layers.Dense(16, activation='relu')(x)
+outputs = tf.keras.layers.Dense(num_classes, activation='softmax')(x)
+
+model = tf.keras.Model(inputs=inputs, outputs=outputs)
+model.summary()
+
+
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate),
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-#train and eval
-model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_test, y_test))
 
-loss, accuracy = model.evaluate(X_test, y_test)
-print(f'Test accuracy: {accuracy * 100:.2f}%')
+History = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_test, y_test))
 
-#SAVE MODEL to .tflite FILE FORMAT
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
-
-with open('model.tflite', 'wb') as f:
-    f.write(tflite_model)
-
-
-
-#=================past this section is just for testing=================================
-
-interpreter = tf.lite.Interpreter(model_content=tflite_model)
-interpreter.allocate_tensors()
-
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-
-
-
-#================sample inference=============================
-sample_input = X_test[0:1]
-interpreter.set_tensor(input_details[0]['index'], sample_input)
-interpreter.invoke()
-output = interpreter.get_tensor(output_details[0]['index'])
-
-print(f'Softmax output probabilities: {output}')
-print(f'Predicted class: {np.argmax(output)}')
